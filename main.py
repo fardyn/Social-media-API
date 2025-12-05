a74aa59f015e8edf6c8929b5a764bc58cbbe1141
fardyn <fa.alizadeh@pm.me>
2025-12-05 04:03:53 +0330

    add find post path opration

--- main.py
+++ main.py
@@ -1,22 +1,64 @@
-from fastapi import Body, FastAPI
+import random
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: int | None = 0
+
+
+my_posts = [
+    {
+        "title": "I like pizza!!!!",
+        "content": "I really love pizza, especially pepperoni, it's the best!",
+        "published": True,
+        "rating": 5,
+        "id": 1,
+    },
+    {
+        "title": "look at my new car",
+        "content": "I just bought a new car, it's a red convertible!",
+        "published": False,
+        "id": 2,
+    },
+    {
+        "title": "testing this social media app",
+        "content": "",
+        "published": True,
+        "rating": 3,
+        "id": 3,
+    }
+]
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "landing page"}
 
 
 @app.get("/posts")
-async def posts():
-    return {"data": "hello!"}
+async def get_posts():
+    return {"data": my_posts}
 
 
-@app.post("/createpost")
-async def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload["title"]}, content: {payload["content"]}"}
+@app.post("/posts")
+async def create_post(post: Post):
+    id : int = random.randint(1, 10000000000)
+    post_dict = post.model_dump()
+    post_dict["id"] = id
+    my_posts.append(post_dict)
+    return {"data": post}
 
+
+@app.get("/posts/{id}")
+async def get_post(id: int):
+    for post in my_posts:
+        if post["id"] == id:
+            return {"post_detail": post}
+    return {"message": "post not found"}
